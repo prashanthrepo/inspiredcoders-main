b4bb7cf75eb7626ef31542cb2e5d37404f6a5202
Prashanth BR <prashanthrepo@gmail.com>
2022-06-29 12:27:54 +0530

    new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>{Component.title}</title>
+        <title></title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
